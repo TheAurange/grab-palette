ca7db570938d202f4a65865c4255b1bf5dffbd6c
Aurange <auragar@users.noreply.github.com>
2021-05-09 00:05:48 -0500

    Update code_DOM.js

--- code_DOM.js
+++ code_DOM.js
@@ -1,38 +1,39 @@
 //Created by Aurange
 
-var can = document.getElementById("canvas");
-var ctx = can.getContext("2d");
+"use strict";
 
 document.getElementById("in").onchange = function(){
-	var file = new FileReader;
+  let can = document.getElementById("canvas");
+  let ctx = can.getContext("2d");
+	let file = new FileReader();
 	file.onload = function(){
-		var input = new Image;
+		let input = new Image();
 		input.onload = function(){
 			can.width = input.width;
 			can.height = input.height;
 
 			ctx.drawImage(input, 0, 0);
 
-			var colors = ctx.getImageData(0, 0, can.width, can.height).data;
+			let colors = ctx.getImageData(0, 0, can.width, can.height).data;
 
 			document.getElementById("in").style.display = "none";
 			document.getElementById("message").style.display = "block";
 
-			worker = new Worker("code_Worker.js");
+			let worker = new Worker("code_Worker.js");
 			worker.postMessage(colors);
 			worker.onmessage = function(e){
 				document.getElementById("in").style.display = "block";
 				document.getElementById("canvas").style.display = "block";
 				document.getElementById("message").style.display = "none";
 
-				var len = e.data.length;
-				var unit = Math.ceil(Math.sqrt(len));
+				let len = e.data.length;
+				let unit = Math.ceil(Math.sqrt(len));
 				can.width = unit;
 				can.height = unit;
-				var cur;
+				let cur;
 
-				for(var i = 0; i < unit; i++){
-					for(var j = 0; j < unit; j++){
+				for(let i = 0; i < unit; i++){
+					for(let j = 0; j < unit; j++){
 						cur = (i * unit) + j;
 
 						if(cur < len){
