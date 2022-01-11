4aebb5c6c3a48624768827cc9e14c0b2ea63a926
Aurange <auragar@users.noreply.github.com>
2022-01-10 19:11:16 -0600

    Update code_DOM.js

--- code_DOM.js
+++ code_DOM.js
@@ -3,35 +3,34 @@
 "use strict";
 
 document.getElementById("in").onchange = function(){
-  let can = document.getElementById("canvas");
-  let ctx = can.getContext("2d");
-  let file = new FileReader();
+  let can = document.getElementById("canvas"), ctx = can.getContext("2d"), file = new FileReader();
+
   file.onload = function(){
     let input = new Image();
+
     input.onload = function(){
       can.width = input.width;
       can.height = input.height;
 
       ctx.drawImage(input, 0, 0);
 
-      let colors = ctx.getImageData(0, 0, can.width, can.height).data;
+      let colors = ctx.getImageData(0, 0, can.width, can.height).data, worker = new Worker("code_Worker.js");
 
       document.getElementById("in").style.display = "none";
       document.getElementById("message").style.display = "block";
 
-      let worker = new Worker("code_Worker.js");
       worker.postMessage(colors);
+
       worker.onmessage = function(e){
+        let len = e.data.length, unit = Math.ceil(Math.sqrt(len)), cur, file = new Blob([e.data.join("\n")], {type: "plain/text"});
+
         document.getElementById("in").style.display = "block";
         document.getElementById("canvas").style.display = "block";
         document.getElementById("message").style.display = "none";
         document.getElementById("download").style.display = "block";
 
-        let len = e.data.length;
-        let unit = Math.ceil(Math.sqrt(len));
         can.width = unit;
         can.height = unit;
-        let cur;
 
         for(let i = 0; i < unit; i++){
           for(let j = 0; j < unit; j++){
@@ -44,7 +43,6 @@ document.getElementById("in").onchange = function(){
           }
         }
 
-        let file = new Blob([e.data.join("\n")], {type: "plain/text"});
         document.getElementById("download").href = URL.createObjectURL(file);
 
         document.getElementById("count").innerText = "Total Colors: " + Number(len).toLocaleString();
@@ -60,6 +58,7 @@ document.getElementById("in").onchange = function(){
   
   this.value = null;
   this.blur();
+
   document.getElementById("canvas").style.display = "none";
   document.getElementById("download").style.display = "none";
   document.getElementById("count").innerText = "";
