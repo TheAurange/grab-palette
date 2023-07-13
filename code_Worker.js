0f83b629824c62d0f5bba9ca58a6dd243bfd0867
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-07-13 13:49:27 -0500

    Update code_Worker.js

--- code_Worker.js
+++ code_Worker.js
@@ -3,12 +3,12 @@
 "use strict";
 
 self.onmessage = function(e){
-  let uniqueColors = new Set(), len = e.length;
+  let uniqueColors = new Set(), len = e.data.length;
 
-  console.log(`The value of e is: ${e}.`);
+  console.log(`The value of e is: ${e.data}.`);
 
   for(let i = 0; i < len; i += 4){
-    uniqueColors.add(`${e[i]}, ${e[i + 1]}, ${e[i + 2]}, ${e[i + 3]}`);
+    uniqueColors.add(`${e.data[i]}, ${e.data[i + 1]}, ${e.data[i + 2]}, ${e.data[i + 3]}`);
   }
 
   postMessage(Array.from(uniqueColors).sort());
