24b51c33badbc2cd30ed814d20164d1ddbc5710b
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-07-13 17:10:36 -0500

    Update code_Worker.js

--- code_Worker.js
+++ code_Worker.js
@@ -9,5 +9,22 @@ self.onmessage = function(e){
     uniqueColors.add(`${e.data[i]}, ${e.data[i + 1]}, ${e.data[i + 2]}, ${e.data[i + 3]}`);
   }
 
-  postMessage(Array.from(uniqueColors).sort());
+  postMessage(Array.from(uniqueColors).sort((a, b) => {
+    let splitA = a.split(", "),
+        splitB = b.split(", ");
+
+    if(Number(splitA[0]) > Number(splitB[0])) return 1;
+    else if(Number(splitA[0]) < Number(splitB[0])) return -1;
+
+    if(Number(splitA[1]) > Number(splitB[1])) return 1;
+    else if(Number(splitA[1]) < Number(splitB[1])) return -1;
+
+    if(Number(splitA[2]) > Number(splitB[2])) return 1;
+    else if(Number(splitA[2]) < Number(splitB[2])) return -1;
+
+    if(Number(splitA[3]) > Number(splitB[3])) return 1;
+    else if(Number(splitA[3]) < Number(splitB[3])) return -1;
+
+    return 0;
+  }));
 }
