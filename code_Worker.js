b527d129c45c986c869f71cc76e9ffda1ecb2a0a
Aurange <11928004+TheAurange@users.noreply.github.com>
2023-07-13 13:34:18 -0500

    Update code_Worker.js

--- code_Worker.js
+++ code_Worker.js
@@ -5,6 +5,8 @@
 self.onmessage = function(e){
   let uniqueColors = new Set(), len = e.length;
 
+  console.log(len);
+
   for(let i = 0; i < len; i += 4){
     uniqueColors.add(`${e[i]}, ${e[i + 1]}, ${e[i + 2]}, ${e[i + 3]}`);
   }
